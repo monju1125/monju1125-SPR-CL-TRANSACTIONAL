072314ebc86c784389e09bf29d4b0fe87ce7799c
Md Monjurul Karim <csekarim86@gmail.com>
2026-06-28 01:48:14 -0400

    Initial commit

---
+++ src/test/java/lab/ShipServiceTest.java
@@ -0,0 +1,29 @@
+package lab;
+
+import Lab.Service.ShipService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.Annotation;
+
+public class ShipServiceTest {
+
+    @Test
+    public void testTransactionalAnnotationPresence() {
+        Class<ShipService> shipServiceClass = ShipService.class;
+        boolean annotationFound = shipServiceClass.isAnnotationPresent(Transactional.class);
+        Assertions.assertTrue(annotationFound);
+    }
+
+    @Test
+    public void testTransactionalRollbackForTonnageException() {
+        Class<ShipService> shipServiceClass = ShipService.class;
+        Annotation transactionalAnnotation = shipServiceClass.getAnnotation(Transactional.class);
+
+        boolean annotationPropertyFound = transactionalAnnotation.toString().contains("InvalidTonnageException");
+
+        Assertions.assertTrue(annotationPropertyFound);
+    }
+
+}
